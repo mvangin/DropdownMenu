62098eb9df11e6913710d00cfba16856aaec7e9e
mvangin <43646116+mvangin@users.noreply.github.com>
2020-06-09 16:22:11 -0700

    webpack error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import nextPic from "./menuDrop.js"
 import slideshow from "./slideshow.js"
+import MenuDrop from "./MenuDrop.js"
 
 
+const navDropTitles = document.querySelectorAll(".navDropTitle");
 
-
+MenuDrop.menuDrop(navDropTitles);
 
 
 
